d52d421ac7a6237d656b5770b6f04721db3a4a62
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2015-08-13 11:04:00 -0700

    Add some intermediates.
    Conflicts:
    	platform_certs/default/gdig2.pem

--- platform_certs/get-issuer.sh
+++ platform_certs/get-issuer.sh
@@ -7,13 +7,13 @@
 # process of adding the intermediate so the checker can validate properly.
 cd $(dirname $0)/default
 DOMAIN="${1}"
-ISSUER_URL=$(openssl s_client -connect "${DOMAIN}:443" -showcerts </dev/null | \
+ISSUER_URL=$(openssl s_client -connect "${DOMAIN}:443" -servername "${DOMAIN}" -showcerts </dev/null | \
   openssl x509 -text -noout | \
   perl -lne 'print $1 if /CA Issuers - URI:(.*)/')
 NAME=$(basename "${ISSUER_URL}" .crt)
 curl "${ISSUER_URL}" | openssl x509 -inform der -outform pem > "${NAME}.pem"
 if openssl verify -CApath . ${NAME}.pem ; then
-  c_rehash .
+#  c_rehash .
   curl --capath . "https://${DOMAIN}" >/dev/null
   echo "Succesfully fetched and verified ${NAME}.pem. Now git add . and commit."
 else
